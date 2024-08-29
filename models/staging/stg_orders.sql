d1968e652cdbd9722938596d7e0f028b054e73fb
kssumiyaroshan@gmail.com <kssumiyaroshan@gmail.com>
2024-08-29 14:26:20 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,19 @@
+select
+o.ordersellingprice - o.ordercostprice as orderprofit ,
+o.ordersellingprice,
+o.ordercostprice,
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid = p.productid
